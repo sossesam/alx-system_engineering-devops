f520b3ad42975129ddab073e1bd7346a47f26313
sossesam <sossesam@gmail.com>
2023-11-25 08:37:43 +0100

    all

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,8 +2,8 @@
 
 file {'/tmp/school':
     ensure  => present,
-    owner  => 'www-data',
-    group  => 'www-data',
-    mode   => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
     content => 'I love Puppet',
 }
