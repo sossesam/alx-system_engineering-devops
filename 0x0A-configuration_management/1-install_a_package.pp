1f3b6669738cef5fd8fcbfd3fb702f550c51f6fe
sossesam <sossesam@gmail.com>
2023-11-25 10:08:27 +0100

    all

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# script install flask version 2.1.0
+# script installs flask version 2.1.0
 
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
