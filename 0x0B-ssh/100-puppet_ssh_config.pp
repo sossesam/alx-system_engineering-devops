9c6d3c27b93bbe949dc06fcd940ab18303cd0e69
sossesam <sossesam@gmail.com>
2023-11-28 07:27:18 +0100

    all

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#using puppet to set up server.
+
+file{'etc/ssh/ssh_config':
+    ensure => present,
+    content => "
+        Include /etc/ssh/ssh_config.d/*.conf
+
+        Host *
+        PasswordAuthentication no
+        IdentityFile ~/.ssh/school
+
+    ",
+
+}
